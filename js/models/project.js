3de5c9da5ccd0756bbf3c54b1fc94a10dd1b28cc
Dana Mulder <danarmulder@gmail.com>
2015-03-26 13:36:10 -0700

    fix projects page and menu

--- js/models/project.js
+++ js/models/project.js
@@ -7,28 +7,35 @@ App.Project = DS.Model.extend({
 });
 
 App.Project.FIXTURES = [{
-  id: 1,
-  name: "Feel it",
+    id: 1,
+    name: "Bootstrap Labs - Intern",
+    image: "images/Dealflow.png",
+    description: "Worked on an internal project to create an investment management platform similar to AngelList.  For this project I learned a new stack, Meteor.js to develop on a pre-existing codebase.  Meteor uses Mongo as a database and is a platform designed for asynchronous data transfer between server and client.  Helped to zero-in on data structure and create a network-based management application.",
+    link: "http://bootstraplabs.com/",
+    technologies: "Meteor, MongoDB"
+}, {
+  id: 2,
+  name: "Feelit.co - Intern",
   image: "images/feelit.png",
   description: "Interned with the company Feel It, a social networking app.  Created an online registration for the mobile app on the website.",
   link: "http://feelit.co/",
   technologies: "Rails, AJAX, JQuery, Facebook API"
 }, {
-  id: 2,
-  name: "Puppy Love",
+  id: 3,
+  name: "Puppy Play Date",
   image: "images/unipug.jpg",
   description: "First version was created on Rails and the front end using Javascript and Google Api.  A second version was created using test-driven development, again on Rails and using backbone as a front end tool.  Users can log in, upload photos, message each other, search for other users.",
   link: "",
   technologies: "Rails, Google API, AJAX, Jquery, Backbone, Unit testing"
 }, {
-  id: 3,
+  id: 4,
   name: "3D Gem Game",
   image: "images/rubygem.png",
   description: "Created a simple click-event game in 3D in javascript.",
   link: "http://danarmulder.github.io/Ruby-gem-game/",
   technologies: "JQuery, Three.js"
 }, {
-  id: 4,
+  id: 6,
   name: "Gcamp",
   image: "images/gcamp.png",
   description: "Remake of Basecamp.  Created with Ruby on Rails.  App has users who can have projects, which can have tasks, projects can also have members who also own the project.",
